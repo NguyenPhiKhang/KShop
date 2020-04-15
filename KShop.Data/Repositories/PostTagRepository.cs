4b70452fbf35e48b90c893fa084b9b7a91d46fb7
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-04-16 01:36:24 +0700

    Revert "Revert "add more repositories for other tables""
    This reverts commit ef389a13621962beabcd573475840207782e6009.

---
+++ KShop.Data/Repositories/PostTagRepository.cs
@@ -0,0 +1,21 @@
+﻿using KShop.Data.Inflastructure;
+using KShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KShop.Data.Repositories
+{
+    public interface IPostTagRepository
+    {
+    }
+
+    public class PostTagRepository : RepositoryBase<PostTag>, IPostTagRepository
+    {
+        public PostTagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ KShop.Data/Repositories/SlideRepository.cs
@@ -0,0 +1,21 @@
+﻿using KShop.Data.Inflastructure;
+using KShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KShop.Data.Repositories
+{
+    public interface ISlideRepository
+    {
+    }
+
+    public class SlideRepository : RepositoryBase<Slide>, ISlideRepository
+    {
+        public SlideRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
