2d9995b3b3a0a3cce0aa92149c818ada468fceb1
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-04-22 11:12:49 +0700

    add dependency injection autofac to web

---
+++ KShop.Web/App_Start/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using KShop.Data;
+using KShop.Data.Inflastructure;
+using KShop.Data.Repositories;
+using KShop.Service;
+using Microsoft.Owin;
+using Owin;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+
+[assembly: OwinStartup(typeof(KShop.Web.App_Start.Startup))]
+
+namespace KShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutoFac(app);
+        }
+
+        private void ConfigAutoFac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<KShopDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+        }
+    }
+}
