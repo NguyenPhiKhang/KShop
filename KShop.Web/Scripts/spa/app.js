6ce9b465e83643aa8f4415ad9c43679a00fa750d
Khang Nguyễn Phi <17520616@ms.uit.edu.vn>
2020-04-24 00:31:56 +0700

    service in angularjs

--- KShop.Web/Scripts/spa/app.js
+++ KShop.Web/Scripts/spa/app.js
@@ -1,19 +1,27 @@
 ﻿var myApp = angular.module("myModule", []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service("Validator", Validator);
 
-//myController.$inject = ["$scope"];
-function schoolController($scope) {
-    $scope.message = "Announcement from school";
+schoolController.$inject = ["$scope", "Validator"];
+
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
 }
 
-//declare 
-function studentController($scope) {
-    $scope.message = "This is my message from Student";
-}
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
 
-function teacherController($scope) {
-    $scope.message = "This is my message from Teacher";
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "this is even";
+        } else {
+            return "this is odd";
+        }
+    }
 }
