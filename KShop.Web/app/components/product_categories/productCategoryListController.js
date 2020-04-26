244bee00f8f64ea7942b348273f3a705ffafaecd
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-04-26 13:12:20 +0700

    paging for list

--- KShop.Web/app/components/product_categories/productCategoryListController.js
+++ KShop.Web/app/components/product_categories/productCategoryListController.js
@@ -7,15 +7,30 @@
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
         $scope.getProductCategories = getProductCategories;
+        $scope.page = 0;
+        $scope.pagesCount = 0;
 
-        function getProductCategories() {
-            apiService.get("/api/productcategory/getall", null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+                }
+            }
+
+            apiService.get("/api/productcategory/getall", config, function (result) {
+                console.log(result.data);
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.totalCount = result.data.TotalCount;
+                $scope.pagesCount = result.data.TotalPages;
             }, function () {
                     console.log("load product category failed");
             });
         }
 
         $scope.getProductCategories();
+
     }
 })(angular.module("kshop.product_categories"));
